0c72637415445c8ff475099d1c6bb59a7f872fcd
Kimur1 <kimur53@gmail.com>
2021-04-08 17:34:33 +0000

    index.html

--- src/js/component/CardPlanet.js
+++ src/js/component/CardPlanet.js
@@ -5,10 +5,7 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<img
-					src="https://logodownload.org/wp-content/uploads/2015/12/star-wars-logo-0.png"
-					alt="logo de Starwars"
-				/>
+				<span className="navbar-brand mb-0 h1">Star Wars</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,7 +4,15 @@ import "../../styles/home.scss";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<h1>Planets</h1>
+		<p>
+			<img src={rigoImage} />
+		</p>
+		<a href="#" className="btn btn-success">
+			If you see this green button, bootstrap is working
+		</a>
+
+		<h1>Characters</h1>
 		<p>
 			<img src={rigoImage} />
 		</p>
